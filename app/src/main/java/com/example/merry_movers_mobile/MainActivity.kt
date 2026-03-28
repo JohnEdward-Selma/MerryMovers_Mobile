2e624e058aab76803012a24c1fd1694c5d9c6a6c
LAB24_USER ON WS38 <L24Y09W38@LabHCI.net>
2026-03-28 10:28:00 +0800

    initial pushing

---
+++ app/src/main/java/com/example/merry_movers_mobile/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.merry_movers_mobile
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.welcome_screen)
+    }
+}
